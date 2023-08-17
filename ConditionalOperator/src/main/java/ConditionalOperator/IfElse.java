d46361082f42d1e0ca3a3e2c7425b833d12eb10c
vimalvppatel <138659897+vimalvppatel@users.noreply.github.com>
2023-08-17 12:23:40 +0530

    Update IfElse.java

--- ConditionalOperator/src/main/java/ConditionalOperator/IfElse.java
+++ ConditionalOperator/src/main/java/ConditionalOperator/IfElse.java
@@ -5,8 +5,8 @@ public class IfElse {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		 int a = 4000;
-		 int b = 1000;
+		 int a = 40000;
+		 int b = 10000;
 		 
 		 if (a>b) {
 			 

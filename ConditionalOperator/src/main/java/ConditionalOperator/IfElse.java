0e549b9ded6672882eea0e6afe334345483cc011
Vimal Patel <vimalpatel35458@gmail.com>
2023-08-17 16:51:50 +0530

    chnge value

--- ConditionalOperator/src/main/java/ConditionalOperator/IfElse.java
+++ ConditionalOperator/src/main/java/ConditionalOperator/IfElse.java
@@ -5,8 +5,8 @@ public class IfElse {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		 int a = 40000;
-		 int b = 10000;
+		 int a = 3000;
+		 int b = 2000;
 		 
 		 if (a>b) {
 			 
